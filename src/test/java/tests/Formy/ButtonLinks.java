bc04ac962c7c963c67c3bc2a94201464937c7fd7
emrahcinar <emrah-c@hotmail.com>
2020-07-09 19:57:32 -0400

    second task case for button links

--- src/test/java/tests/Formy/ButtonLinks.java
+++ src/test/java/tests/Formy/ButtonLinks.java
@@ -1,5 +1,6 @@
 package tests.Formy;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormyHomePage;
 import utils.Driver;
@@ -17,6 +18,18 @@ public class ButtonLinks {
         clickOnTheDesiredLink1("Buttons");
 
         Driver.get().close();
-
     }
+
+        @Test
+        public void verify_button_link_page_title() throws Exception {
+
+            FormyHomePage formyHomePage = new FormyHomePage();
+            Driver.get().get(formyHomePage.url);
+
+            clickOnTheDesiredLink1("Buttons");
+            Assert.assertEquals(Driver.get().getCurrentUrl().contains("buttons"),true);
+            Driver.get().close();
+
+
+        }
 }
